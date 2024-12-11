90cb31e7f9764dac2b8451f442d48f3e8368230e
Liam Galley <119807591+LiamGalley@users.noreply.github.com>
2024-12-11 15:43:13 -0500

    Footer Completed.

--- galley-portfolio/src/App.tsx
+++ galley-portfolio/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './scss/root/root.scss'
 import NavBar from './components/nav-bar'
+import Footer from './components/footer'
+import emailIcon from './assets/Black_Email_SVG.svg'
+import githubIcon from './assets/Black_Github_SVG.svg'
+import linkedinIcon from './assets/Black_LinkedIn_SVG.svg'
+import websiteIcon from './assets/Black_WWW_SVG.svg'
+
+// IMAGES ATTRIBUTIONS TO https://www.iconfinder.com/search/icons?family=eon 
+const iconList = [
+  { svg: githubIcon, link: "https://github.com/LiamGalley", alt: "Github Icon" },
+  { svg: linkedinIcon, link: "https://www.linkedin.com/in/liam-galley/", alt: "LinkedIn Icon" },
+  { svg: websiteIcon, link: "...", alt: "WWW Icon" },
+  { svg: emailIcon, link: "...", alt: "Email Icon" }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <NavBar title='LG'></NavBar>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Footer iconList={iconList}></Footer>
     </>
   )
 }
